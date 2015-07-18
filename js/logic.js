5ecbae90b5c4069fbe6567ce6e4bd9e009419643
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2015-07-17 18:34:41 -0700

    fixed ajax stuff...almost

--- js/logic.js
+++ js/logic.js
@@ -1,27 +1,54 @@
-/*
-	
-*/	
+
+
+
 var urlbase = "http://api.wordnik.com:80/v4/word.json/";
-var urlFinish = "/relatedWords?useCanonical=false&limitPerRelationshipType=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";	
-	
-for(var i = 0; i < data.length; i++){
-	var word = data[i].word;
-	var urlSynonyms= urlbase + word + urlFinish;
-	function getSynonyms(word){
-	    $.get(urlSynonyms, function(word, status){
-	        var synonymsArray = _.findWhere(word, {relationshipType: "synonym"});
-	        var synonymsString = synonymsArray.words.join(", ");
-	        console.log(synonymsString);
-	        return synonymsString;
-	    });
-	}
-	data[i].synonyms = getSynonyms(word);
-	console.log(data[i].synonyms);
-	
-}	
+var urlFinish = "/relatedWords?useCanonical=false&limitPerRelationshipType=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+var numErrors = 0;
 
 
 
+for (var i = 0; i < data.length; i++) {
+    var word = data[i].word;
+    var urlSynonyms = urlbase + word + urlFinish;
+
+	//var throttleIt = _.throttle(getSynonyms, 100);
+	 
+    function getSynonyms(word) {
+        
+        $.get(urlSynonyms, function(wordIter, status) {
+            var synonymsArray = _.findWhere(wordIter, {
+                relationshipType: "synonym"
+            });
+			if (synonymsArray === undefined){
+				return false;
+			}
+			else{
+	            var synonymsString = synonymsArray.words.join(", ");
+	            console.log(synonymsString);
+	        	return synonymsString;
+			}
+            
+            
+        });
+    }   
+    var syn = getSynonyms();
+    if (syn){
+	    data[i].synonyms = syn;
+	    //data[i].synonyms = throttleIt(word); 
+	    console.log("word:" + data[i].synonyms);
+    }
+    else{
+	    console.warn(syn);
+	    data[i].synonyms = "No Notable Synonyms for " + word;
+    }
+}
+
+
+console.log(numErrors);
+// var synonymsString = synonymsArray.words.join(", "); // this line is occassionally erroring with "words" no clue wtf but whatevs 
+// issue?
+// are try and catch actual keywords? that I've just missed somehow? wtf
+// Yeah - they exist - and so does this model - in most languages
 
 
 function newWordSet(){
@@ -35,7 +62,7 @@ function newWordSet(){
 		}
 	word();	
 	$("#word").html("<div class='front text-center'><br><h1>" + data[randomNumber].word + "</h1></div>");
-	$("#definition").html("<div class='back'><h3 style='color:darkgrey;'>" + data[randomNumber].definition_value + "</h3></div>");
+	$("#definition").html("<div class='back'><h3 style='color:darkgrey;'>" + data[randomNumber].definition_value + "<br><small>" + data[randomNumber].synonyms + "</small></h3></div>");
 	
 }
 	$('#new-word').on('click', function(event) {        
@@ -53,22 +80,11 @@ for(var i = 0; i < data.length; i++){
 		console.log(word);
 		var definition = data[i].definition_value;
 		var wordlink = data[i].word_link;
+		var wordSyns = data[i].synonyms;
 		var numbering = i + 1;
-		$("#all").append( "<h3>"+ numbering + ". " + word + "</h3><p>" + definition + "<br>" + "<a href='" + wordlink + "' target='_blank'>Full Definition</a></p>");
+		$("#all").append( "<h3>"+ numbering + ". " + word + "</h3><p>" + definition + "<br>" +  wordSyns +"<br>" + "<a href='" + wordlink + "' target='_blank'>Full Definition</a></p>");
 	}
 /* +*/
 
 });
-
-
-
-/*
-function getAndIterateData() {					
-		_.each(data, function(datum){ 
-		    input = datum.word; 	   
-			return input;
-	    });  
-	}
-
-*/
 
